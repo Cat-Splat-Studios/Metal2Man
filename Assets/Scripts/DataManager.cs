33460d567b550959a942321eb5e62d40699c044e
franco <chilenoo92@gmail.com>
2021-06-12 10:01:19 -0400

    Adding Instructions for DataManager

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 /// <summary>
 /// - Adding data to the manager
 /// - Accessing the data from the manager
-/// 
+///  (Example of use: A station needs to know if player is pressing interact button; instead of GetComponent<Player>().IsInteracting
+/// with the Data Manager it would be DataManager.MakeItRain<Player>(DataKeys.LOCALPLAYER).isInteracting.
+/// In order to achieve this, there are two steps:
+/// 1: Set up a data key entry to your class
+/// 2: Register the class to the data manager in your Awake() with DataManager.ToTheCloud(newDataKey, this). 
 /// </summary>
 public static class DataManager
 {
